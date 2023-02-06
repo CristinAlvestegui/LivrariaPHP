99d9c52b995e0c8a5a65c96fac7ef7fc792b2724
Cristina Alvéstegui <104037804+CristinAlvestegui@users.noreply.github.com>
2023-02-06 18:22:40 -0300

    Update
    Adicionando função para consultar todos os livros, ainda não foi testado 📚

--- PHP/Modelo/DAO/ConsuLivro.php
+++ PHP/Modelo/DAO/ConsuLivro.php
@@ -7,24 +7,46 @@
 
     class ConsuLivro{
 
-        public function consulIndi(Conexao $connect, string $Livros, int $codigo){
+        public function consulIndi(Conexao $connect, string $Livros, int $ISBN){
             try{
                 $conn = $connect->conectar();
                 $sql = "select * from $Livros where ISBN = '$ISBN'";
                 $result = mysqli_query($conn, $sql);
-                mysqli_close($conn);
+                
                 while($dados = mysqli_fetch_Array($result)){
                     if($dados["ISBN"] == $ISBN){
-                        echo "<br>ISBN: ".$dados["ISBN"]."<br>Titulo: ".$dados["titulo"]."<br>Autor: ".$dados["autor"]."<br>Editora: ".$dados["editora"]."Lançamento: ".$dados["data"];
+                        echo "<br>ISBN: ".$dados["ISBN"]."<br>Titulo: ".$dados["titulo"]."<br>Autor: ".$dados["autor"]."<br>Editora: ".$dados["editora"]."Lançamento: ".$dados["data"]."<br>Editora: ".$dados["editora"]."Lançamento: ".$dados["data"]."<br><br>";
                         return;//return vazio só para sair do if
                     }//Fim do if
+                    echo "ISBN não foi encontrado! :(";
                 }//Fim do while
-                echo "ISBN não foi encontrado! :(";
+                mysqli_close($conn);
+                
             }
             catch(Except $erro){
                 echo $erro;
+            }//Fim do Try Catch
+        }//Fim do método para consultar Livro individual
+
+        public function consulTudo(Conexao $connect, string $Livros){
+            try{
+                $conn = $connect->conectar();
+                $sql = "select * from $Livros";
+                $result = mysqli_query($conn, $sql);
+                
+                while($dados = mysqli_fetch_Array($result)){
+                    if($dados["ISBN"] == $ISBN){
+                        echo "<br>ISBN: ".$dados["ISBN"]."<br>Titulo: ".$dados["titulo"]."<br>Autor: ".$dados["autor"]."<br>Editora: ".$dados["editora"]."Lançamento: ".$dados["data"]."<br><br>";
+                        return;//return vazio só para sair do if
+                    }//Fim do if
+                }//Fim do while
+                mysqli_close($conn);
+                
             }
-        }
-    }//Fim da classe para consultar individualmente
+            catch(Except $erro){
+                echo $erro;
+            }//Fim do Try Catch
+        }//Fim do método para Consultar todos os livros
+    }//Fim da classe para consultar Livro
 
 ?>
